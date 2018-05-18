2162f473df405cb785547317f39aeb9380a66bf0
Benjamin Thurm <bentrm@posteo.de>
2018-05-18 09:52:14 +0200

    Only redirect to https if not in debug mode

--- src/lis/settings.py
+++ src/lis/settings.py
@@ -49,19 +49,20 @@ EMAIL_USE_SSL = env("EMAIL_USE_SSL", True) in ("True", "true", "t", "1", True)
 EMAIL_SUBJECT_PREFIX = "[LIS] "
 
 # SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = env("VIRTUAL_HOST", "localhost").split(",")
 DEBUG = env("DEBUG", True) in ("True", "true", "t", "1", True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", "***SECRET***")
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = "DENY"
 
-ALLOWED_HOSTS = env("VIRTUAL_HOST", "localhost").split(",")
+if not DEBUG:
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = "DENY"
 
 # Application definition
 
