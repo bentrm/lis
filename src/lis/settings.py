3d66479b2d4f7891ae7c7f3c70de01a0ed4bf74c
Benjamin Thurm <bentrm@posteo.de>
2018-05-24 14:09:58 +0200

    Revert debug default to false

--- src/lis/settings.py
+++ src/lis/settings.py
@@ -50,7 +50,7 @@ EMAIL_SUBJECT_PREFIX = "[LIS] "
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = env("VIRTUAL_HOST", "localhost").split(",")
-DEBUG = env("DEBUG", True) in ("True", "true", "t", "1", True)
+DEBUG = env("DEBUG", False) in ("True", "true", "t", "1", True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", "***SECRET***")
