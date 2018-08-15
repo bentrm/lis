ec7364027640a4b6df25d8ed6f0ff85ad444d7ce
Benjamin Thurm <bentrm@posteo.de>
2018-08-15 14:35:08 +0200

    Remove obsolete home app

--- src/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import IndexView, ImpressumView
-
-app_name = "home"
-urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
-    path("impressum/", ImpressumView.as_view(), name="impressum"),
-]
