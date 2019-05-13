66cd16384a9bac51e03b13c62b4aaad05141ab30
Benjamin Thurm <bentrm@posteo.de>
2019-05-13 13:43:52 +0200

    Make backup/restore a bit more resiliant

--- backup.sh
+++ backup.sh
@@ -3,27 +3,34 @@
 set -o nounset
 set -o errexit
 
-# Docker Host settings
-DOCKER_MEDIA_VOLUME='lis_media'
-
-DOCKER_DB_SERVICE='lis_db'
-POSTGRES_DB='django'
-
 # Backup settings
 SUFFIX=$(date '+%Y-%m-%d-%H-%M-%S')
 BACKUP_DIR='backups'
-MEDIA_ARCHIVE="$BACKUP_DIR/media_$SUFFIX.tgz"
-DUMP_FILE="$BACKUP_DIR/dump_$SUFFIX.sql"
-
-# Local Docker settings
-MEDIA_ROOT='files/media'
 
 # Backup files
-docker run --rm --volume "$DOCKER_MEDIA_VOLUME:/backup" busybox sh -c 'tar c -vOz -f - -C /backup --exclude ./images ./' > "$MEDIA_ARCHIVE"
+DOCKER_MEDIA_SERVICE='lis_media'
+MEDIA_ARCHIVE="$BACKUP_DIR/media_$SUFFIX.tgz"
+
+echo "Archiving media files to $MEDIA_ARCHIVE..."
+docker exec -i \
+    $(docker ps --filter name=$DOCKER_MEDIA_SERVICE --format "{{.ID}}") \
+    sh -c "cd /usr/share/nginx/html/media && tar --exclude ./images -cf - ." > $MEDIA_ARCHIVE
 
 # Backup and restore database locally
+DUMP_FILE="$BACKUP_DIR/dump_$SUFFIX.sql"
+POSTGRES_DB='django'
+POSTGRES_USER='django'
+DOCKER_DB_SERVICE='lis_db'
+DOCKER_DB_CONTAINER=$(docker ps --filter name=$DOCKER_DB_SERVICE --format "{{.ID}}")
+
+DOCKER_DJANGO_SERVICE='lis_django'
+DOCKER_DB_CONTAINER=$(docker ps --filter name=$DOCKER_DJANGO_SERVICE --format "{{.ID}}")
+
+echo "Pruning renditions..."
+docker exec -i -u postgres $DOCKER_DB_CONTAINER \
+    /venv/bin/python manage.py prunerenditions
+
 echo "Dumping database to $DUMP_FILE"
 touch $DUMP_FILE
-docker exec -it -u postgres \
-    $(docker ps --filter name=$DOCKER_DB_SERVICE --format "{{.ID}}") \
-    pg_dump --clean --if-exists -d $POSTGRES_DB > "$DUMP_FILE"
+docker exec -i -u postgres $DOCKER_DB_CONTAINER \
+    pg_dump -Fc -U $POSTGRES_USER -d $POSTGRES_DB > "$DUMP_FILE"
--- restore.sh
+++ restore.sh
@@ -1,56 +1,42 @@
 #!/bin/bash
 
 set -o nounset
-set -o errexit
+#set -o errexit
 
 # Backup settings
 BACKUP_DIR=$(realpath 'backups')
-DUMP_FILE=$(ls -t $BACKUP_DIR/dump_* | head -1)
-MEDIA_ARCHIVE=$(ls -t $BACKUP_DIR/media_* | head -1)
-
-# Local docker settings
-DOCKER_DB_VOLUME='lis_pgdata'
-DOCKER_MEDIA_VOLUME='lis_media'
-
-POSTGRES_IMAGE='mdillon/postgis:11-alpine'
-POSTGRES_USER='django'
-POSTGRES_DB='django'
-DB_RESTORE_CONTAINER_NAME='lis-restore-db'
-
-MEDIA_IMAGE='nginx:alpine'
-MEDIA_RESTORE_CONTAINER_NAME='lis-restore-media'
-MEDIA_ROOT='files/media'
-
-if [[ $(docker ps -a --filter volume=$DOCKER_DB_VOLUME --format "{{.ID}}") ]]; then
-    echo "Volume $DOCKER_DB_VOLUME is in use."  1>&2
-    exit 1
-fi
 
 # Restore database
-docker run --rm -d --name $DB_RESTORE_CONTAINER_NAME \
-    --volume "$DUMP_FILE:/dump.sql" \
-    --volume "$DOCKER_DB_VOLUME:/var/lib/postgresql/data" \
-    -e POSTGRES_USER=$POSTGRES_USER \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-    $POSTGRES_IMAGE
-sleep 3  # Wait for database
-docker exec -it $DB_RESTORE_CONTAINER_NAME \
-    psql -U $POSTGRES_USER -d postgres \
-    -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$POSTGRES_DB' AND pid <> pg_backend_pid();"
-docker exec -it $DB_RESTORE_CONTAINER_NAME \
-    psql -U $POSTGRES_USER -d postgres \
-    -c "DROP DATABASE $POSTGRES_DB;"
-docker exec -it $DB_RESTORE_CONTAINER_NAME \
-    psql -U $POSTGRES_USER -d postgres \
-    -c "CREATE DATABASE $POSTGRES_DB;"
-docker exec -it $DB_RESTORE_CONTAINER_NAME \
-    psql -U $POSTGRES_USER -d $POSTGRES_DB -f /dump.sql
-docker stop $DB_RESTORE_CONTAINER_NAME
+DUMP_FILE=$(ls -t $BACKUP_DIR/dump_* | head -1)
+POSTGRES_USER='django'
+POSTGRES_DB='django'
+DOCKER_DB_SERVICE='lis_db'
+DOCKER_DB_CONTAINER=$(docker ps --filter name=$DOCKER_DB_SERVICE --format "{{.ID}}")
 
-docker run --rm -d --name $MEDIA_RESTORE_CONTAINER_NAME \
-    --volume "$MEDIA_ARCHIVE:/media.tar" \
-    --volume "$DOCKER_MEDIA_VOLUME:/usr/share/nginx/html/media" \
-    $MEDIA_IMAGE
-docker exec -it $MEDIA_RESTORE_CONTAINER_NAME \
-    tar -xvf /media.tar -C /usr/share/nginx/html/media
-docker stop $MEDIA_RESTORE_CONTAINER_NAME
+echo 'Terminating all connections...'
+docker exec -i -u postgres $DOCKER_DB_CONTAINER \
+    psql -U $POSTGRES_USER -d postgres \
+        -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$POSTGRES_DB' AND pid <> pg_backend_pid();"
+
+echo 'Dropping existing database...'
+docker exec -i $DOCKER_DB_CONTAINER \
+    dropdb --if-exists -U $POSTGRES_USER $POSTGRES_DB
+
+echo "Restoring database from $DUMP_FILE..."
+docker exec -i $DOCKER_DB_CONTAINER \
+    pg_restore --create --no-owner -U $POSTGRES_USER -d postgres < $DUMP_FILE
+
+# Restore media files
+MEDIA_ARCHIVE=$(ls -t $BACKUP_DIR/media_* | head -1)
+DOCKER_MEDIA_SERVICE='lis_media'
+DOCKER_MEDIA_CONTAINER=$(docker ps --filter name=$DOCKER_MEDIA_SERVICE --format "{{.ID}}")
+
+echo "Copying media archive $MEDIA_ARCHIVE to running container $DOCKER_MEDIA_CONTAINER..."
+docker cp $MEDIA_ARCHIVE $DOCKER_MEDIA_CONTAINER:/backup.tgz
+
+echo "Restoring media archive..."
+docker exec -i $DOCKER_MEDIA_CONTAINER \
+    tar -C /usr/share/nginx/html/media -xvf /backup.tgz
+
+echo "Cleanup..."
+docker exec -i $DOCKER_MEDIA_CONTAINER rm /backup.tgz
