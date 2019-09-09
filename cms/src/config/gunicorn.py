d821c4e77678963af0c604d5acc0afa7a8ee5b09
Benjamin Thurm <bentrm@posteo.de>
2019-09-09 14:07:20 +0200

    Allow to specify number of WSGI workers

--- cms/src/config/gunicorn.py
+++ cms/src/config/gunicorn.py
@@ -7,4 +7,5 @@ bind = "0.0.0.0:8000"
 disable_redirect_access_to_syslog = True
 errorlog = "-"
 reload = env("DEBUG", default=False, parse_to_bool=True)
-workers = multiprocessing.cpu_count() * 2 + 1
+max_number_workers = int(env("MAX_WORKERS", default=3))
+workers = min(multiprocessing.cpu_count() * 2 + 1, max_number_workers)
