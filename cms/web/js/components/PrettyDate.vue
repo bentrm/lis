982527d4ae4ea39e0bd3ed6ac94d723992a1bb01
Benjamin Thurm <bentrm@posteo.de>
2019-10-10 10:23:01 +0200

    Pretty date component

---
+++ cms/web/js/components/PrettyDate.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>{{ formattedDate }}</span>
+</template>
+
+<script>
+  import {currentLanguage} from '../translate';
+
+
+  export default {
+    props: {
+      day: Number,
+      month: Number,
+      year: Number,
+      place: String
+    },
+
+    computed: {
+      formattedDate () {
+        const vm = this;
+        let output = '';
+        if (vm.year && vm.month && vm.day) {
+          output += new Date(vm.year, (vm.month - 1), vm.day).toLocaleDateString(currentLanguage);
+        }
+        if (vm.place) {
+          output += ` (${vm.place})`;
+        }
+        return output;
+      }
+    }
+  }
+</script>
