b902e7510c791c345958e3803bf512bca4c107ca
Benjamin Thurm <bentrm@posteo.de>
2019-11-05 09:52:25 +0100

    fix: Make switching language compatible with SPA urls
    temp

--- cms/src/lis/urls.py
+++ cms/src/lis/urls.py
@@ -1,14 +1,64 @@
 """Application wide URL config."""
+import logging
+from urllib.parse import unquote
 
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import include, path, re_path
+from django.utils.http import is_safe_url
+from django.utils.translation import (LANGUAGE_SESSION_KEY, check_for_language)
 from django.views.generic import TemplateView
+from django.views.i18n import LANGUAGE_QUERY_PARAMETER
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+logger = logging.getLogger(__name__)
+
+
+def set_language(request):
+    """
+    Redirect to a given URL while setting the chosen language in the session
+    (if enabled) and in a cookie. The URL and the language code need to be
+    specified in the request parameters.
+
+    Since this view changes how the user will see the rest of the site, it must
+    only be accessed as a POST request. If called as a GET request, it will
+    redirect to the page in the request (the 'next' parameter) without changing
+    any state.
+    """
+    next = request.POST.get('next', request.GET.get('next'))
+    host = request.get_host()
+    is_secure = request.is_secure()
+
+    if ((next or not request.is_ajax()) and
+            not is_safe_url(url=next, allowed_hosts={host}, require_https=is_secure)):
+        next = request.META.get('HTTP_REFERER')
+        next = next and unquote(next)  # HTTP_REFERER may be encoded.
+        if not is_safe_url(url=next, allowed_hosts={host}, require_https=is_secure):
+            logger.warning(f"Referrer {next} not safe to redirect, falling back to '/'")
+            next = '/'
+
+    response = HttpResponseRedirect(next) if next else HttpResponse(status=204)
+    if request.method == 'POST':
+        lang_code = request.POST.get(LANGUAGE_QUERY_PARAMETER)
+        if lang_code and check_for_language(lang_code):
+            if hasattr(request, 'session'):
+                request.session[LANGUAGE_SESSION_KEY] = lang_code
+            response.set_cookie(
+                settings.LANGUAGE_COOKIE_NAME, lang_code,
+                max_age=settings.LANGUAGE_COOKIE_AGE,
+                path=settings.LANGUAGE_COOKIE_PATH,
+                domain=settings.LANGUAGE_COOKIE_DOMAIN,
+            )
+        else:
+            logger.warning(f"Language code {lang_code} not found.")
+
+    return response
+
+
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('i18n/setlang/', set_language, name='set_language'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('wagtail/', include(wagtail_urls)),
     path('admin/', include(wagtailadmin_urls)),
