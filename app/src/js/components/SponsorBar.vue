ad412a4a4739a9d4f63fa72870291c02315efdd5
Benjamin Thurm <bentrm@posteo.de>
2019-11-12 11:49:49 +0100

    fix: Make sponsor log play nice with blocker

--- app/src/js/components/SponsorBar.vue
+++ app/src/js/components/SponsorBar.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="navbar navbar-light">
     <span class="small d-none d-sm-block">
-      Gefördert durch<br>
-      Podpořeno
+      Gefördert durch
+      <br />Podpořeno
     </span>
 
-    <a class="navbar-brand ml-md-auto" href="http://www.sn-cz2020.eu/" target="_blank" rel="noopener">
-      <img :src="euLogo" alt="Logo EU Fonds" class="sponsor-logo img img-fluid">
-      <img :src="snczLogo" alt="Logo SN CZ" class="sponsor-logo img img-fluid">
+    <a
+      class="navbar-brand ml-md-auto"
+      href="http://www.sn-cz2020.eu/"
+      target="_blank"
+      rel="noopener"
+    >
+      <img :src="euLogo" alt="Logo EU Fonds" class="eu-logo img img-fluid" />
+      <img :src="snczLogo" alt="Logo SN CZ" class="eu-logo img img-fluid" />
     </a>
   </div>
 </template>
 
 <script>
-  import euLogo from '../../assets/logos/logo_eu.svg';
-  import snczLogo from '../../assets/logos/logo_sn_cz.svg';
+import euLogo from '../../assets/logos/logo_eu.svg';
+import snczLogo from '../../assets/logos/logo_sn_cz.svg';
 
-
-  export default {
-    data () {
-      return {
-        euLogo,
-        snczLogo
-      };
-    }
+export default {
+  data() {
+    return {
+      euLogo,
+      snczLogo
+    };
   }
+};
 </script>
 
 <style lang="scss">
-  .sponsor-logo {
-    max-height: 2.5rem;
-  }
+.eu-logo {
+  max-height: 2.5rem;
+}
 </style>
