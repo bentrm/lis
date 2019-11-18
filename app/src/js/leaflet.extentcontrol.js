fb42619b76c7cdb614298aac772d5d8ec8870a7e
Benjamin Thurm <bentrm@posteo.de>
2019-11-18 14:35:50 +0100

    feat: Refactor leaflet extent control to es6

--- app/src/js/leaflet.extentcontrol.js
+++ app/src/js/leaflet.extentcontrol.js
@@ -1,37 +1,43 @@
-import L from 'leaflet';
+import translate from './translate';
+import { DomUtil, DomEvent, Control } from 'leaflet';
 
-export default L.Control.extend({
+export default Control.extend({
 
   options: {
-    title: 'Zoom to feature extent',
+    title: translate('Zoom to feature extent'),
     layer: null
   },
 
-  onAdd (map) {
+  setLayer(layer) {
+    this.options.layer = layer;
+  },
+
+  onAdd(map) {
+
     const extentName = 'leaflet-control-extent';
-    const container = L.DomUtil.create('div', extentName + ' leaflet-bar');
+    const container = DomUtil.create('div', extentName + ' leaflet-bar');
     const options = this.options;
 
     this._map = map;
 
-    this._link = L.DomUtil.create('a', extentName + '-btn', container);
+    this._link = DomUtil.create('a', extentName + '-btn', container);
     this._link.href = '#';
     this._link.title = options.title;
     this._link.setAttribute('role', 'button');
     this._link.setAttribute('aria-label', options.title);
 
-    L.DomUtil.create('i', 'fas fa-expand-arrows-alt', this._link);
+    DomUtil.create('i', 'fas fa-expand-arrows-alt', this._link);
 
-    L.DomEvent
-      .on(this._link, 'click', L.DomEvent.stopPropagation)
-      .on(this._link, 'click', L.DomEvent.preventDefault)
+    DomEvent
+      .on(this._link, 'click', DomEvent.stopPropagation)
+      .on(this._link, 'click', DomEvent.preventDefault)
       .on(this._link, 'click', this._onClick, this)
-      .on(this._link, 'dblclick', L.DomEvent.stopPropagation);
+      .on(this._link, 'dblclick', DomEvent.stopPropagation);
 
     return container;
   },
 
-  _onClick () {
+  _onClick() {
     if (this.options.layer.getLayers().length) {
       this._map.fitBounds(this.options.layer.getBounds());
     }
