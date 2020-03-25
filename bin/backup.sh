621c0386e88e2a932fed994c8283b340cdee8d39
Benjamin Thurm <bentrm@posteo.de>
2020-03-25 09:36:32 +0100

    feat: Make backup/restore more resilient

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,36 +3,46 @@
 set -o nounset
 set -o errexit
 
+if [ $# -lt 2 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo 1>&2 "usage: $0 path env"
+  exit 2
+elif [ $# -gt 2 ]; then
+  echo 1>&2 "$0: too many arguments"
+  echo 1>&2 "usage: $0 path env"
+  exit 2
+fi
+
+# Backup settings
+SUFFIX=$(date '+%Y-%m-%d-%H-%M-%S')
+BACKUP_DIR="$1"
+ENV_FILE="$2"
+DUMP_FILE="${BACKUP_DIR}dump_$SUFFIX.sql"
+MEDIA_ARCHIVE="${BACKUP_DIR}media_$SUFFIX.tgz"
+
 while IFS= read -r line
 do
   IFS='=' read -r temp val <<< "$line"
   printf -v $temp "$val"
   export $temp
-done < "$1"
-
-# Backup settings
-SUFFIX=$(date '+%Y-%m-%d-%H-%M-%S')
-BACKUP_DIR='backups'
-DUMP_FILE="$BACKUP_DIR/dump_$SUFFIX.sql"
-MEDIA_ARCHIVE="$BACKUP_DIR/media_$SUFFIX.tgz"
+done < "$ENV_FILE"
 
 # Backup files
-DOCKER_STATIC_CONTAINER=$(docker-compose ps -q static)
+DOCKER_STATIC_CONTAINER=$(docker ps -q -f LABEL=com.docker.compose.project=lis -f LABEL=com.docker.compose.service=static)
 
 echo "Archiving media files to $MEDIA_ARCHIVE..."
-docker exec -i $DOCKER_STATIC_CONTAINER \
-    sh -c "cd /usr/share/nginx/html/media && tar --exclude ./images -cf - ." > $MEDIA_ARCHIVE
+docker exec -i "$DOCKER_STATIC_CONTAINER" \
+    sh -c "cd /usr/share/nginx/html/media && tar --exclude ./images -cf - ." > "$MEDIA_ARCHIVE"
 
 echo "Pruning renditions..."
-DOCKER_CMS_CONTAINER=$(docker-compose ps -q cms)
-docker exec -i $DOCKER_CMS_CONTAINER \
+DOCKER_CMS_CONTAINER=$(docker ps -q -f LABEL=com.docker.compose.project=lis -f LABEL=com.docker.compose.service=cms)
+docker exec -i "$DOCKER_CMS_CONTAINER" \
     python manage.py prunerenditions
-
 
 echo "Dumping database to $DUMP_FILE"
 POSTGRES_DB=$DB_NAME
 POSTGRES_USER=$DB_USER
-DOCKER_DB_CONTAINER=$(docker-compose ps -q db)
-touch $DUMP_FILE
-docker exec -i $DOCKER_DB_CONTAINER \
-    pg_dump -Fc -U $POSTGRES_USER -d $POSTGRES_DB > "$DUMP_FILE"
+DOCKER_DB_CONTAINER=$(docker ps -q -f LABEL=com.docker.compose.project=lis -f LABEL=com.docker.compose.service=db)
+touch "$DUMP_FILE"
+docker exec -i "$DOCKER_DB_CONTAINER" \
+    pg_dump -Fc -U "$POSTGRES_USER" -d "$POSTGRES_DB" > "$DUMP_FILE"
--- bin/restore.sh
+++ bin/restore.sh
@@ -3,49 +3,61 @@
 set -o nounset
 set -o errexit
 
+if [ $# -lt 3 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo 1>&2 "usage: $0 path timestamp env"
+  exit 2
+elif [ $# -gt 3 ]; then
+  echo 1>&2 "$0: too many arguments"
+  echo 1>&2 "usage: $0 path timestamp env"
+  exit 2
+fi
+
+# Backup settings, defaults to newest files
+BACKUP_DIR="$1"
+TIMESTAMP="$2"
+ENV_FILE="$3"
+DUMP_FILE="$BACKUP_DIR"dump_"$TIMESTAMP".sql
+MEDIA_ARCHIVE="$BACKUP_DIR"media_"$TIMESTAMP".tgz
+
 while IFS= read -r line
 do
   IFS='=' read -r temp val <<< "$line"
   printf -v $temp "$val"
   export $temp
-done < "$1"
-
-# Backup settings
-BACKUP_DIR=$(realpath 'backups')
+done < "$ENV_FILE"
 
 # Restore database
-DUMP_FILE=$(ls -t $BACKUP_DIR/dump_* | head -1)
 POSTGRES_DB=$DB_NAME
 POSTGRES_USER=$DB_USER
-DOCKER_DB_CONTAINER=$(docker-compose ps -q db)
+DOCKER_DB_CONTAINER=$(docker ps -q -f LABEL=com.docker.compose.project=lis -f LABEL=com.docker.compose.service=db)
 
 echo 'Terminating all connections...'
-docker exec -i -u postgres $DOCKER_DB_CONTAINER \
-    psql -U $POSTGRES_USER -d postgres \
+docker exec -i -u postgres "$DOCKER_DB_CONTAINER" \
+    psql -U "$POSTGRES_USER" -d postgres \
         -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$POSTGRES_DB' AND pid <> pg_backend_pid();"
 
 echo 'Dropping existing database...'
-docker exec -i $DOCKER_DB_CONTAINER \
-    dropdb --if-exists -U $POSTGRES_USER $POSTGRES_DB
+docker exec -i "$DOCKER_DB_CONTAINER" \
+    dropdb --if-exists -U "$POSTGRES_USER" "$POSTGRES_DB"
 
 echo "Restoring database from $DUMP_FILE..."
-docker exec -i $DOCKER_DB_CONTAINER \
-    pg_restore --create --no-owner -U $POSTGRES_USER -d postgres < $DUMP_FILE
+docker exec -i "$DOCKER_DB_CONTAINER" \
+    pg_restore --create --no-owner -U "$POSTGRES_USER" -d postgres < "$DUMP_FILE"
 
 # Restore media files
-MEDIA_ARCHIVE=$(ls -t $BACKUP_DIR/media_* | head -1)
-DOCKER_APP_CONTAINER=$(docker-compose ps -q cms)
+DOCKER_APP_CONTAINER=$(docker ps -q -f LABEL=com.docker.compose.project=lis -f LABEL=com.docker.compose.service=cms)
 
 echo "Copying media archive $MEDIA_ARCHIVE to running container $DOCKER_APP_CONTAINER..."
-docker cp $MEDIA_ARCHIVE $DOCKER_APP_CONTAINER:/backup.tgz
+docker cp "$MEDIA_ARCHIVE" "$DOCKER_APP_CONTAINER":/backup.tgz
 
 echo "Restoring media archive..."
-docker exec -i $DOCKER_APP_CONTAINER \
+docker exec -i "$DOCKER_APP_CONTAINER" \
     tar -C /html/media -xvf /backup.tgz
 
 echo "Cleanup..."
-docker exec -i $DOCKER_APP_CONTAINER sh -c "rm /backup.tgz"
+docker exec -i "$DOCKER_APP_CONTAINER" sh -c "rm /backup.tgz"
 
 echo "Pruning renditions..."
-docker exec -i $DOCKER_APP_CONTAINER \
+docker exec -i "$DOCKER_APP_CONTAINER" \
     python manage.py prunerenditions
